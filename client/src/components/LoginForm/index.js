f721c9c66889ba66ae1df107dfe80b6e965e3be6
Eladio Montenegro <eladio.montenegro@gmail.com>
2019-11-16 13:38:53 -0600

    created the parent login page and added local storage of their username

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import API from "../../utils/API.js";
 import "./style.css";
+import Button from "../CustomButtons/Button.js";
 
 
 
@@ -10,10 +11,9 @@ class LoginForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentStep: 1,
-      username:null,
-      password:null
-
+      username:" ",
+      password:" ",
+      selectedUser:{}
     }
   }
 
@@ -29,12 +29,36 @@ class LoginForm extends React.Component {
   handleSubmit = event => {
     event.preventDefault()
     //enter login info here
-    const { username,password} = this.state
+    const {username,password} = this.state
 
 
     if (this.state.username && this.state.password) {
       //you can put api calls here
       console.log("they exist!");
+      API.getParents()
+      .then(  res => {
+
+          this.setState();
+          //for loop, same response to new array 
+          let parentArray = res.data;
+
+          var i;
+          for (i = 0; i < parentArray.length; i++) {
+            if( parentArray[ i ].username === this.state.username ){
+              this.state.selectedUser = parentArray[ i ];
+          console.log (this.state.selectedUser);
+          localStorage.setItem("id", parentArray[i]._id);
+          
+        
+          window.location.href = "/parentportal";
+     
+              break;
+                
+          ;
+}}//delete this at some point 
+        console.log (res.data);
+      }) 
+      .catch(err => console.log(err));
   }
 }
   
@@ -43,8 +67,36 @@ class LoginForm extends React.Component {
   render() {    
     return (
       <React.Fragment>
-          <p>just some text</p>
+        <div className="form-group">
+        <label htmlFor="username">Username</label>
+        <input
+          className="form-control"
+          id="username"
+          name="username"
+          type="text"
+          placeholder="Enter username"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+      </div>
 
+
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          className="form-control"
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Enter password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+      </div>
+      <div style={{marginTop:"30px"}} >
+      <Button color="primary" type="button" onClick={this.handleSubmit}> SAVE</Button>
+      <div id="notice" style={{display:"none"}}><h3>Profile Saved!</h3></div>
+      </div>
       
       </React.Fragment>
 
